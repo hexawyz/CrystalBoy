a3ff4ce3e7a4ef9f41a6e67ecf7058b8f596f1b2
GoldenCrystal <GoldenCrystal@users.noreply.github.com>
2011-05-28 15:32:54 +0000

    Switch to VS2010 Fixed the Direct3D RenderMethod assembly to x86 architecture. Added a SlimDX/Direct3D 9 RenderMethod for those with 64 bits system. Now ignore BadImageFormatException while loading plugin assemblies.

---
+++ CrystalBoy.Emulation.Rendering.SlimDX/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("CrystalBoy.Emulation.Rendering.SlimDX")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CrystalBoy.Emulation.Rendering.SlimDX")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("72985425-e3cb-424c-9569-555ec4bf2896")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CrystalBoy.Emulation.Rendering.SlimDX/TransformedTexturedVertex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SlimDX;
+
+namespace CrystalBoy.Emulation.Rendering.SlimDX
+{
+	internal struct TransformedTexturedVertex
+	{
+		public Vector4 Position;
+		public Vector2 TextureCoordinates;
+
+		public TransformedTexturedVertex(Vector4 position, Vector2 textureCoordinates)
+		{
+			Position = position;
+			TextureCoordinates = textureCoordinates;
+		}
+
+		public const int StrideSize = 4 * sizeof(float) + 2 * sizeof(float);
+	}
+}
