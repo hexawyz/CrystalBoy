595d0aa301843a864ab6f8fff9d328eaf6355b93
goldencrystal@gmail.com <goldencrystal@gmail.com@cd4401a2-aa89-11dd-907f-95f1ebde4186>
2014-07-09 19:31:49 +0000

    Adding the missing files, with slight updates to the rendering architecture…

---
+++ trunk/CrystalBoy.Emulation/VideoBufferReference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalBoy.Emulation
+{
+	/// <summary>Represents a reference to a video buffer.</summary>
+	public struct VideoBufferReference
+	{
+		/// <summary>Pointer to the image data.</summary>
+		public readonly IntPtr DataPointer;
+		/// <summary>The offset in bytes between two rows of pixels in the buffer.</summary>
+		/// <remarks>This value may be negative.</remarks>
+		public int Stride;
+
+		public VideoBufferReference(IntPtr dataPointer, int stride)
+		{
+			this.DataPointer = dataPointer;
+			this.Stride = stride;
+		}
+	}
+}
---
+++ trunk/CrystalBoy.Emulator.Rendering.SharpDX/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿#region Copyright Notice
+// This file is part of CrystalBoy.
+// Copyright © 2008-2011 Fabien Barbier
+// 
+// CrystalBoy is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CrystalBoy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CrystalBoy.Emulation.Rendering.SharpDX")]
+[assembly: AssemblyDescription("CrystalBoy SharpDX-based renderers.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5c016ea9-04d6-41c3-814d-f96973f50cc3")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
